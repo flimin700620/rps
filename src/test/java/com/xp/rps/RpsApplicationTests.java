40b15fc11efd2c4391ccbddbc041c574550840a5
Ubuntu <ubuntu@ip-172-31-44-194.ap-southeast-1.compute.internal>
2020-10-27 02:26:46 +0000

    tset

--- src/test/java/com/xp/rps/RpsApplicationTests.java
+++ src/test/java/com/xp/rps/RpsApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
